a1e7b7c3580e222d2d75ea40499a9a91d1fffb51
Olivier D'Hallewin <olivier.dhallewin@gmail.com>
2018-03-22 21:49:19 +0100

    Add cors library

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var cors = require('cors')
+
 
 // create express app
 var app = express();
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors())
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
